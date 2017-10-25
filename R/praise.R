71c13fc8c36499b2bc4e7a5422d406ddac40fb2b
ellesaber <ellen.saber@gmail.com>
2017-10-25 15:21:17 +1100

    Add some examples for users

--- R/praise.R
+++ R/praise.R
@@ -3,6 +3,12 @@
 #' Praise me at critical times in my life
 #'
 #' @export
+#'
+#' @examples
+#'
+#' # I'm going to delivery some praise!
+#' praise()
+#'
 praise <- function() {
   print("You're the best!")
 }
